e87392df198b25d559bcb65fe307a659fa17cb85
LuoisVeasna <161813651+LuoisVeasna@users.noreply.github.com>
2024-10-25 13:34:49 +0200

    Add files via upload

---
+++ ModuleOne.py
@@ -0,0 +1,13 @@
+total_amount =2000
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+#If running not exit resulf
+if __name__=='__main':
+    print(__name__)
+print('Fro, Module One', add(20,30))
+print('From Module One', sub(50,10))
